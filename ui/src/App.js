ff3a81b6703d3b9027d2c98743898fe1cd1dddd3
David Budnick <david@budnick.ca>
2018-11-18 00:43:33 -0600

    Redux Setup

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
+import Posts from './components/Posts';
+import PostForm from './components/PostForm';
+import store from './store';
+
 import './App.css';
+import 'bulma/css/bulma.css';
+require('dotenv').config();
+// import { Route, withRouter } from 'react-router-dom';
+// import SecuredRoute from './SecuredRoute/SecuredRoute';
+// import auth0Client from './Auth';
+// import Callback from './Callback';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <PostForm />
+            <hr />
+            <Posts />
+          </header>
+        </div>
+      </Provider>
     );
   }
 }

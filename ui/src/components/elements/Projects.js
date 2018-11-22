3473c35cc5d75237183a5bd7e6ba1ffb5b61d417
David Budnick <david@budnick.ca>
2018-11-22 14:48:23 -0600

    New Project style

--- ui/src/components/elements/Projects.js
+++ ui/src/components/elements/Projects.js
@@ -2,31 +2,37 @@
 import { connect } from 'react-redux';
 import { getUser } from '../../actions/userActions';
 import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-
 class Projects extends Component {
   componentWillMount() {
-    console.log(this.props.location.pathname.substring(9));
     this.props.getUser(this.props.location.pathname.substring(9));
   }
-
   render() {
     return (
       <div>
-        <p className="control mt">
-          <b />
-          <b />
-          <b />
-          <b />
-          <a className="">Hey! {this.props.user.fullName}</a>
-        </p>
+        <div className="columns is-multiline">
+          <div className="column is-narrow">
+            <Link to="/newProject">
+              <div className="box has-text-centered addBox ml mt">
+                <i className="fas fa-plus is-size-5 newProject" />
+              </div>
+            </Link>
+          </div>
+          <div className="column is-narrow">
+            <Link to={{ pathname: this.props.user.userId + '/project/' + '1' }}>
+              <div className="box has-text-centered addBox ml mt">
+                <div className="newProject">Project Name</div>
+              </div>
+            </Link>
+          </div>
+        </div>
       </div>
     );
   }
 }
 Projects.propTypes = { getUser: propTypes.func.isRequired };
 const mapStateToProps = (state) => ({ user: state.user.user });
-
 export default connect(
   mapStateToProps,
   { getUser },

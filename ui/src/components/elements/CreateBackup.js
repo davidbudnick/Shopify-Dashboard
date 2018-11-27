7da5d486ce9a85dc6b72190f53e32014eddf1527
David Budnick <david@budnick.ca>
2018-11-27 14:08:06 -0600

    Backups work on click

--- ui/src/components/elements/CreateBackup.js
+++ ui/src/components/elements/CreateBackup.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
+import propTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createBackup } from '../../actions/backupActions';
 
 export class CreateBackup extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onSubmit(e) {
+    e.preventDefault();
+    this.props.createBackup(this.props.projectId);
+  }
   render() {
     return (
       <div className="column">
-        <div className="button mt ml is-info" onClick={this.props.createBackup(this.props.projectId)}>
-          Backup Products
-        </div>
+        <form onSubmit={this.onSubmit}>
+          <button type="submit" className="button mt ml is-info">
+            Backup Products
+          </button>
+        </form>
       </div>
     );
   }
 }
+
+CreateBackup.propTypes = {
+  createBackup: propTypes.func.isRequired,
+};
+
 export default connect(
   null,
   { createBackup },

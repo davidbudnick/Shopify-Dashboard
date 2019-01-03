2ff0fd48563e03366a9e292b41ed44771c828fe0
David Budnick <david@budnick.ca>
2019-01-02 21:56:41 -0600

    Fixed deploy script for server

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,5 @@
-dbudnick/shopify-dashboard-ui
-dbudnick/shopify-dashbaord-api
+docker pull dbudnick/shopify-dashboard-ui
+docker pull dbudnick/shopify-dashbaord-api
 docker-compose down
 docker-compose up -d
 docker-compose logs -f

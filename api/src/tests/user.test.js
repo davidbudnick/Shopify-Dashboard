441867c27533844b663c5d46f8d41b0c4d739df2
David Budnick <david@budnick.ca>
2019-01-21 14:34:24 -0600

    Updated tests and gitignore

--- api/src/tests/user.test.js
+++ api/src/tests/user.test.js
@@ -1,24 +1,18 @@
 const user = require('../user');
 
-// describe('User Tests', () => {
-//   it('should return the user object', () => {
-//     expect(user.getUser('google-oauth2|101062415965177315471')).toBe({
-//       id: 2,
-//       userId: 'google-oauth2|101062415965177315471',
-//       firstName: 'David',
-//       lastName: 'Budnick',
-//       fullName: 'David Budnick',
-//       nickName: 'david',
-//       picture:
-//         'https://lh5.googleusercontent.com/-qBpnZLwiRT0/AAAAAAAAAAI/AAAAAAAAAAA/AKxrwcZUGhAxms1-qw497V6c69BKPwCdUA/mo/photo.jpg',
-//       createdAt: '2019-01-19T23:09:34.351Z',
-//       updatedAt: '2019-01-19T23:09:34.351Z',
-//     });
-//   });
-// });
-
-test('the data is peanut butter', () => {
-  return user.getUser().then((data) => {
-    expect(data).toBe('peanut butter');
+describe('User Tests', () => {
+  it('should return the user object', () => {
+    expect(user.getUser('google-oauth2|101062415965177315471')).toBe({
+      id: 2,
+      userId: 'google-oauth2|101062415965177315471',
+      firstName: 'David',
+      lastName: 'Budnick',
+      fullName: 'David Budnick',
+      nickName: 'david',
+      picture:
+        'https://lh5.googleusercontent.com/-qBpnZLwiRT0/AAAAAAAAAAI/AAAAAAAAAAA/AKxrwcZUGhAxms1-qw497V6c69BKPwCdUA/mo/photo.jpg',
+      createdAt: '2019-01-19T23:09:34.351Z',
+      updatedAt: '2019-01-19T23:09:34.351Z',
+    });
   });
 });

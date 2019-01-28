29f339705edf1ac9ce34b4d03ad3385279376ea3
David Budnick <david@budnick.ca>
2019-01-28 13:26:43 -0600

    Removed examples of actions and reducers

--- ui/src/actions/types.js
+++ ui/src/actions/types.js
@@ -1,7 +1,3 @@
-//Example exports
-export const FETCH_POSTS = 'FETCH_POSTS';
-export const NEW_POST = 'NEW_POST';
-
 //Shopify exports
 export const GET_PRODUCTS = 'GET_PRODUCTS';
 
--- ui/src/reducers/index.js
+++ ui/src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import postReducer from './postReducer';
 import shopifyReducer from './shopifyReducer';
 import userReducer from './userReducer';
 import projectReducer from './projectReducer';
 import backupReducer from './backupReducer';
 
 export default combineReducers({
-  posts: postReducer,
   shopify: shopifyReducer,
   user: userReducer,
   projects: projectReducer,
